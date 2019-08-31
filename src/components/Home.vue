cccbee77c13d0e60982b1469494a7114aecbe772
rootsuke <rootsuke@gmail.com>
2019-08-31 22:47:03 +0900

    sec 7-1

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        status: "critical"
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.status = "normal"
+      }
+    }
+  }
+</script>
