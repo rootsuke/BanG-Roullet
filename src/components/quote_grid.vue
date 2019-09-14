5649aefadfa95e84f71a85995986b7b59eb46549
rootsuke <rootsuke@gmail.com>
2019-09-14 18:36:09 +0900

    finished sec10

---
+++ src/components/quote_grid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, i) in quotes" :key="i" @click.native="deleteQuote(i)">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './quote.vue'
+  export default {
+    props: ['quotes'],
+    components: {
+      appQuote: Quote
+    },
+    methods: {
+      deleteQuote(i) {
+        this.$emit('quoteDeleted', i)
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
