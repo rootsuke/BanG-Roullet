20bdf39740b097dd47fee56219752bb7b1c63eda
rootsuke <rootsuke@gmail.com>
2019-09-20 00:34:35 +0900

    add function to draw a roullet

--- src/components/roullet.vue
+++ src/components/roullet.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <canvas id="canvas"></canvas>
+        color: <input type="text" v-model="element" @keypress.enter="add_element_to_roullet">
+        {{roullet_elements}}
     </div>
 </template>
 
@@ -8,15 +10,43 @@
     export default {
         data() {
             return {
-                doc: null
+                canvas_width: 300,
+                canvas_height: 300,
+                context: null,
+                element: "",
+                roullet_elements: []
             }
         },
         mounted() {
             const canvas = document.getElementById('canvas')
-            const ctx = canvas.getContext('2d');
-            ctx.fillStyle = 'green';
-            ctx.fillRect(10, 10, 150, 100);
-        }
+            canvas.width = this.canvas_width;
+            canvas.height = this.canvas_height;
+            this.context = canvas.getContext('2d');
+        },
+        methods: {
+            add_element_to_roullet() {
+                // 要素をルーレットに追加
+                var el = {title: this.element}
+                this.roullet_elements.push(el)
+                this.element = ""
+                // ルーレットを描画
+                this.draw_roullet();
+            },
+            draw_roullet() {
+                const context = this.context
+                // ルーレットの描画が重複しないように初期化する
+                context.clearRect(0, 0, this.canvas_width, this.canvas_height)
+                var deg = 0;
+                this.roullet_elements.forEach(el => {
+                    context.beginPath()
+                    context.moveTo(150, 150);
+                    context.fillStyle = el.title;
+                    context.arc(150, 150, 100, deg / 180 * Math.PI, (deg + 90) / 180 * Math.PI, false);
+                    context.fill()
+                    deg += 90
+                })
+            }
+        },
     }
 </script>
 
