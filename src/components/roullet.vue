af5a46d5859038c1c0372760d2ab711413fb0bae
rootsuke <rootsuke@gmail.com>
2019-09-23 02:15:23 +0900

    add pointer and label

--- src/components/roullet.vue
+++ src/components/roullet.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
-        <canvas id="canvas"></canvas>
-        <input type="text" v-model="element" @keypress.enter="add_element_to_roullet">
-        <button v-if="roullet_elements.length != 0 && !isStart" @click="start_roullet()">start</button>
-        <button v-if="isStart" @click="stop_roullet()">stop</button>
+        <div>
+            <canvas id="canvas"></canvas>
+        </div>
+        <div>
+            <input type="text" v-model="element" @keypress.enter="add_element_to_roullet">
+            <button v-if="roullet_elements.length != 0 && !isStart" @click="start_roullet()">start</button>
+            <button v-if="isStart" @click="stop_roullet()">stop</button>
+        </div>
+        <ul>
+            <li v-for="(element, i) in roullet_elements" :key="i">
+                <span :style="{'background-color': element.color}" class="label"></span>
+                {{ element.title }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -28,8 +38,28 @@
             canvas.width = this.canvas_width;
             canvas.height = this.canvas_height;
             this.context = canvas.getContext('2d');
+            this.init_roullet();
         },
         methods: {
+            init_roullet() {
+                const context = this.context;
+                context.beginPath();
+                context.fillStyle = this.colors[0].color;
+                context.arc(150, 150, 100, 0, 2 * Math.PI, false);
+                context.fill();
+                this.draw_pointer();
+            },
+            draw_pointer() {
+                const context = this.context;
+                context.beginPath();
+                context.moveTo(150, 55);
+                context.lineTo(155, 30);
+                context.lineTo(145, 30);
+                context.closePath();
+                context.fillStyle = 'black';
+                context.stroke();
+                context.fill();
+            },
             add_element_to_roullet() {
                 // 要素を25個までに制限する
                 if (this.roullet_elements.length >= 25) {
@@ -47,22 +77,25 @@
                 const context = this.context
                 // ルーレットの描画が重複しないように初期化する
                 context.clearRect(0, 0, this.canvas_width, this.canvas_height)
-                // 描画開始座標を90度ずらす
+                // ルーレットの描画開始座標を90度ずらす
                 offset -= 90
                 const len = this.roullet_elements.length
                 // ルーレットの要素の角度
                 const deg_per_el = 360 / len
 
                 for(let i = 0; i < len; i++) {
-                    const el = this.roullet_elements[i]
+                    const el = this.roullet_elements[i];
+                    const color = this.colors[i].color;
+                    el.color = color;
                     context.beginPath()
                     context.moveTo(150, 150);
-                    context.fillStyle = this.colors[i].color;
+                    context.fillStyle = color;
                     context.arc(150, 150, 100, offset / 180 * Math.PI, (offset + deg_per_el) / 180 * Math.PI, false);
                     context.fill()
                     // 次のルーレットの要素の描画開始座標を更新
                     offset += deg_per_el
                 }
+                this.draw_pointer();
             },
             start_roullet() {
                 let offset = 0
@@ -110,4 +143,13 @@
 </script>
 
 <style>
+    li {
+        list-style: none;
+    }
+
+    .label {
+        width: 15px;
+        height: 15px;
+        display: inline-block;
+    }
 </style>
