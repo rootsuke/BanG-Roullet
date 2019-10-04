41318c555e41ed7331d4b0916a218e947006362c
rootsuke <rootsuke@gmail.com>
2019-10-05 00:40:38 +0900

    create color style file

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,14 @@ module.exports = {
     watchOptions: {
       poll: 5000
     }
+  },
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('scss').oneOfs.store;
+    oneOfsMap.forEach(item => {
+      item.use('sass-resources-loader')
+          .loader('sass-resources-loader')
+          .options({ resources: './src/assets/style/colors.scss' })
+          .end();
+    });
   }
 }
